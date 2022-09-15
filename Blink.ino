7e33a83fb1254a8e396416c00e1ebb43cb3fbef3
Madhav <103959778+therock297@users.noreply.github.com>
2022-09-16 01:24:28 +0530

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,81 +1,193 @@
-//2110994819
-//Madhav Mittal
-
-//The setup function runs once when we press reset or power the board
-void setup()
+void dot() 
 {
-  //Initializing digital pin LED_BUILTIN as an output
-  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for 1 second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
 }
- 
-//blinks a dot
-void dot()
+void dash() 
 {
-  digitalWrite(LED_BUILTIN, HIGH); //Turning the LED on by giving HIGH voltage
-  delay(100);  //waits for 0.1 seconds
-  digitalWrite(LED_BUILTIN, LOW); //Turning the LED off by giving LOW voltage
-  delay(100);  //waits for 0.1 seconds
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(3000);                       // wait for 3 second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
 }
 
-//It blinks a dash
-void dash()
-{
-  digitalWrite(LED_BUILTIN, HIGH); //Turning the LED on by giving HIGH voltage
-  delay(300); //wait for 0.3 seconds
-  digitalWrite(LED_BUILTIN, LOW); //Turning the LED off by giving LOW voltage
-  delay(300); //wait for 0.3 seconds
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
-//Function for giving longer break for a new word
-void new_word()
-{
-  delay(500);  //wait for 0.5 seconds--
-}
+// the loop function runs over and over again forever
+void loop() {
+  String name = Serial.readString();
+  name.toUpperCase();
+  for (int i = 0; i < name.length(); i++) {
+    switch (name[i])
+    {
+      case 'A':
+        dot();
+        dash();
+        break;
+      case 'B':
+        dash();
+        dot();
+        dot();
+        dot();
+        break;
 
-//This loop function runs over and over again forever
-void loop() 
-{
-  //In this loop we are calling the dash() and dot() function for each specific letter. 
-  //It will blink letter M on Arduino Nano
-  //M(--)
-  dash();
-  dash();
-  new_word();
+      case 'C':
+        dash();
+        dot();
+        dash();
+        dot();
+        break;
 
-  //It will blink letter A on Arduino Nano
-  //A(.-)
-  dot();
-  dash();
-  new_word();
+      case 'D':
+        dash();
+        dot();
+        dot();
+        break;
 
-  //It will blink letter D on Arduino Nano
-  //D(-..)
-  dash();
-  dot();
-  dot();
-  new_word();
+      case 'E':
+        dot();
+        break;
 
-  
-  //It will blink letter H on Arduino Nano
-  //H(....)
-  dot();
-  dot();
-  dot();
-  dot();
-  new_word();
+      case 'F':
+        dot();
+        dot();
+        dash();
+        dot();
+        break;
 
-  //It will blink letter A on Arduino Nano
-  //A(.-)
-  dot();
-  dash();
-  new_word();
+      case 'G':
+        dash();
+        dash();
+        dot();
+        break;
 
-  //It will blink letter V on Arduino Nano
-  //v(...-)
-  dot();
-  dot();
-  dot();
-  dash();
-  new_word();
+      case 'H':
+        dot();
+        dot();
+        dot();
+        dot();
+        break;
 
+      case 'I':
+        dot();
+        dot();
+        break;
+      case'J':
+        dot();
+        dash();
+        dash();
+        dash();
+        break;
+
+      case 'K':
+        dash();
+        dot();
+        dash();
+        break;
+
+      case 'L':
+        dot();
+        dash();
+        dot();
+        dot();
+        break;
+
+      case 'M':
+        dash();
+        dash();
+        break;
+
+      case 'N':
+        dash();
+        dot();
+        break;
+
+      case 'O':
+        dash();
+        dash();
+        dash();
+        break;
+
+      case 'P':
+        dot();
+        dash();
+        dash();
+        dot();
+        break;
+
+      case 'Q':
+
+        dash();
+        dash();
+        dot();
+        dash();
+        break;
+
+      case  'R':
+        dot();
+        dash();
+        dot();
+        break;
+
+      case 'S':
+        dot();
+        dot();
+        dot();
+        break;
+
+      case 'T':
+        dash();
+        break;
+
+      case 'U':
+        dot();
+        dot();
+        dash();
+        break;
+
+      case 'V':
+        dot();
+        dot();
+        dot();
+        dash();
+        break;
+
+      case 'W':
+        dot();
+        dash();
+        dash();
+        break;
+
+
+      case 'X':
+        dash();
+        dot();
+        dot();
+        dash();
+        break;
+
+      case 'Y':
+        dash();
+        dot();
+        dash();
+        dash();
+        break;
+
+      case 'Z':
+        dash();
+        dash();
+        dot();
+        dot();
+        break;
+      case ' ':
+        dot();
+        break;
+    }
+  }
 }
